b0a4234d4927bffd415251d371de567403ba7acf
eliasamarillo <eliasamarilloortiz@gmail.com>
2024-12-30 09:26:25 +0100

    actalizacion

--- src/main/kotlin/Ej11_20/Ej11.kt
+++ src/main/kotlin/Ej11_20/Ej11.kt
@@ -6,5 +6,36 @@ package Ej11_20
  * Cualquiera de ellos puede ser el mayor
  */
 fun main() {
+    println("Introduce el primer numero")
+    val num1 = read()
+    println("introduce el numero 2")
+    val num2 = read()
 
+    println(
+        num1?.let { num1 ->
+            num2?.let { num2 ->
+                println(
+                    """Elige una opcion 
+                    |1.Num pares
+                    |2.Num impares
+                    |3.Salir
+                """.trimIndent()
+                )
+                val answ = readln()
+                val (menor, mayor) = order(num1, num2)
+
+                when (answ) {
+                    "1" -> pares(menor, mayor).joinToString()
+                    "2" -> impares(menor, mayor).joinToString()
+                    else -> "Lfin"
+
+                }
+            }
+        }
+    )
 }
+
+fun read(): Int? = readln().toIntOrNull()
+fun pares(a: Int, b: Int): List<Int> = (a..b).filter { it % 2 == 0 }
+fun impares(a: Int, b: Int): List<Int> = (a..b).filter { it % 2 != 0 }
+fun order(a: Int, b: Int) = if (b > a) a to b else b to a

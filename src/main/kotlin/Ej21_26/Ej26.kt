268d5f828951d63dbe7efeec9c8fa53a67886e41
eliasamarillo <eliasamarilloortiz@gmail.com>
2025-01-08 19:06:01 +0100

    actualizacion ejercicios

---
+++ src/main/kotlin/Ej21_26/Ej26.kt
@@ -0,0 +1,8 @@
+package Ej21_26
+
+/**
+ * Diseñar un algoritmo que lea dos números, A y B, y un operador (mediante una variable de tipo carácter), y
+ * calcule el resultado de operar A y B con esa operación. Por ejemplo, si A = 5 y B = 2, y operación = "+", el
+ * resultado debe ser 7. El algoritmo debe seguir pidiendo números y operaciones indefinidamente, hasta que el
+ * usuario decida terminar (utiliza un valor centinela para ello)
+ */
